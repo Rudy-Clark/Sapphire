dba1ba5e9433cb04407937b1adf1ad5598dc98a0
Rudy Clark <rudy.uzb@gmail.com>
2019-05-22 21:29:38 +0500

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 
+import apiPosts from './routes/posts';
+
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 const router = new Router();
@@ -9,7 +11,7 @@ router.get('/', async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.use(router.routes());
+app.use(apiPosts.routes());
 
 export default app.listen(PORT, () => {
   console.log(`App run on http://localhost:${PORT}`);
