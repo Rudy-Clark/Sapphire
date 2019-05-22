a746ce7e13d8722664689986179d089611a98c1c
Rudy Clark <rudy.uzb@gmail.com>
2019-05-22 18:57:09 +0500

    fix eslint errors

--- src/db/migrations/20190512151539_users.js
+++ src/db/migrations/20190512151539_users.js
@@ -1,13 +1,19 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', (table) => {
+exports.up = knex =>
+  knex.schema.createTable('users', table => {
     table.increments();
-    table.string('email').unique().notNullable();
+    table
+      .string('email')
+      .unique()
+      .notNullable();
     table.string('password').notNullable();
-    table.string('username').unique().notNullable();
-    table.boolean('admin').notNullable().defaultTo(false);
+    table
+      .string('username')
+      .unique()
+      .notNullable();
+    table
+      .boolean('admin')
+      .notNullable()
+      .defaultTo(false);
   });
-};
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
-};
+exports.down = knex => knex.schema.dropTable('users');
--- src/db/migrations/20190512151549_posts.js
+++ src/db/migrations/20190512151549_posts.js
@@ -1,16 +1,19 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('posts', (table) => {
+exports.up = knex =>
+  knex.schema.createTable('posts', table => {
     table.increments();
-    table.integer('author_id').unsigned().notNullable();
+    table
+      .integer('author_id')
+      .unsigned()
+      .notNullable();
     table.string('title', 30).notNullable();
     table.text('content').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-    table.foreign('author_id').references('id').inTable('users');
+    table
+      .foreign('author_id')
+      .references('id')
+      .inTable('users');
   });
-};
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('posts');
-};
+exports.down = knex => knex.schema.dropTable('posts');
