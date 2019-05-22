a51b1a58c752aa0cd1658457fcac71eb64feff37
Rudy Clark <rudy.uzb@gmail.com>
2019-05-22 17:52:53 +0500

    install dependencies

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const PORT = process.env.PORT || 1337;
+const router = new Router();
+
+router.get('/', async ctx => {
+  ctx.body = "Hello World";
+});
+
+app.use(router.routes());
+
+export default app.listen(PORT, () => {
+  console.log(`App run on http://localhost:${PORT}`);
+});
