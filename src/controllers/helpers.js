b3b19ba936424037111f14660a6570e9f43c9f6f
Rudy Clark <rudy.uzb@gmail.com>
2019-05-29 16:48:01 +0500

    update errorParser

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -1,20 +1,34 @@
 export function parseError(err) {
   const res = {};
+  console.log(err);
   const { data } = err;
   Object.keys(data).forEach(field => {
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < data[field].length; i++) {
-      if (data[field][i].keyword === 'required') {
-        res[field] = `${field} не может быть пустым`;
-      } else if (data[field][i].keyword === 'minLength') {
-        res[field] = `${field} не меньше 4 символов`;
-      } else if (data[field][i].keyword === 'maxLength') {
-        res[field] = `${field} не больше 100 символов`;
-      } else if (data[field][i].keyword === 'unique') {
-        res[field] = `${data[field][i].value} уже занят`;
+      switch (data[field][i].keyword) {
+        case 'required':
+          res[field] = `${field} не может быть пустым`;
+          break;
+        case 'minLength':
+          res[field] = `${field} не меньше 4 символов`;
+          break;
+        case 'maxLength':
+          res[field] = `${field} не больше 100 символов`;
+          break;
+        case 'unique':
+          res[field] = `${data[field][i].value} уже занят`;
+          break;
+        case 'format':
+          res[field] = `неверный формат E-mail`;
+          break;
+        default:
+          break;
       }
     }
   });
-
+  /**
+   * FORMAT
+   * res = { field: [{ keyword: @String, value: @String }] }
+   */
   return res;
 }
--- tests/routes.auth.test.js
+++ tests/routes.auth.test.js
@@ -152,4 +152,5 @@ describe('routes : users', () => {
         });
     });
   });
+  
 });
