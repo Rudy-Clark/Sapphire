0407699fd5c48ced3913c30dc5305db844415ef5
Rudy Clark <rudy.uzb@gmail.com>
2019-05-30 20:03:36 +0500

    add GET /user/posts

--- src/controllers/user-posts.js
+++ src/controllers/user-posts.js
@@ -1,9 +1,10 @@
 import Router from 'koa-router';
 import passport from 'koa-passport';
+import { isEmpty } from 'lodash';
 
 import Posts from '../models/Posts';
 
-const router = new Router({ prefix: '/users' });
+const router = new Router({ prefix: '/user' });
 
 router.use(async (ctx, next) =>
   passport.authenticate('jwt', (err, user) => {
@@ -11,14 +12,35 @@ router.use(async (ctx, next) =>
       ctx.redirect('/auth/status');
     }
     // eslint-disable-next-line no-return-assign
-    return next((ctx.user = user));
+    return next((ctx.state.user = user));
   })(ctx, next),
 );
 
 router.get('/posts', async ctx => {
   try {
-    const posts = await Posts.query();
+    const { user } = ctx.state;
+    if (isEmpty(user)) throw new Error('Unauthenticated');
+    const posts = await Posts.query()
+      .select('id', 'title', 'content', 'created_at', 'updated_at')
+      .where('author_id', user.id);
     ctx.body = { status: 'success', posts };
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { status: 'error', msg: error };
+  }
+});
+
+router.get('/posts/:id', async ctx => {
+  try {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+    if (isEmpty(user)) throw new Error('Unauthenticated');
+    const posts = await Posts.query()
+      .select('id', 'title', 'content', 'created_at', 'updated_at')
+      .where('author_id', user.id)
+      .where('id', id)
+      .first();
+    ctx.body = { status: 'success', posts: posts || null };
   } catch (error) {
     ctx.status = 500;
     ctx.body = { status: 'error', msg: error };
