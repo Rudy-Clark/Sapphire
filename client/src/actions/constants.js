c13acf67cebc63aa9d0911d8603460860789f3ae
Rudy Clark <rudy.uzb@gmail.com>
2019-06-01 10:10:33 +0500

    add constants

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -1,7 +1,16 @@
 export const ROUTE_CHANGE = '@@router/LOCATION_CHANGE';
+
 // request
 export const REQUEST = 'REQUEST';
 export const REQUEST_SUCCESS = 'REQUEST_SUCCESS';
 export const REQUEST_ERROR = 'REQUEST_ERROR';
+
 // page
 export const SET_PAGE = 'SET_PAGE';
+
+// user
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_UP = 'SIGN_UP';
+export const LOGOUT = 'LOGOUT';
+export const ADD_USER = 'ADD_USER';
+export const DEL_USER = 'DEL_USER';
