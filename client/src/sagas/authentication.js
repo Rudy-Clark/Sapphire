dc7416e0310a0f0717ee0e6783b2bac72e410315
Rudy Clark <rudy.uzb@gmail.com>
2019-06-01 10:13:45 +0500

    add navigation sagas

---
+++ client/src/sagas/authentication.js
@@ -0,0 +1,3 @@
+import { take, put, call, fork, cancel } from 'redux-saga/effects';
+import { isEmpty } from 'lodash';
+
--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,32 +1,7 @@
 /* eslint-disable no-constant-condition */
-import { take, put, call, fork, select, all, cancel } from 'redux-saga/effects';
-import { isEmpty } from 'lodash';
+import { fork, all } from 'redux-saga/effects';
 
-import { ROUTE_CHANGE, REQUEST, REQUEST_SUCCESS } from '../actions/constants';
-import { setReqError, setContent } from '../actions';
-import { loadPage } from '../api';
-
-function* execLoad(fetch) {
-  if (!fetch) yield cancel();
-  yield put({ type: REQUEST });
-  try {
-    const response = yield call(fetch);
-    if (isEmpty(response)) yield put(setReqError('Server Error'));
-    yield put(setContent(response));
-    yield put({ type: REQUEST_SUCCESS });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function* watchNav() {
-  while (true) {
-    const { payload } = yield take(ROUTE_CHANGE);
-    const fetch = yield loadPage(payload);
-    yield fork(execLoad, fetch);
-    console.log(payload);
-  }
-}
+import watchNav from './navigation';
 
 function* rootSaga() {
   yield all([fork(watchNav)]);
