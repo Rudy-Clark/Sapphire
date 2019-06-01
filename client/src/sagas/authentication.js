b58e93a3392c80277a20256b029970bc949da6f8
Rudy Clark <rudy.uzb@gmail.com>
2019-06-01 11:27:38 +0500

    add user auth sagas

--- client/src/sagas/authentication.js
+++ client/src/sagas/authentication.js
@@ -1,3 +1,31 @@
-import { take, put, call, fork, cancel } from 'redux-saga/effects';
+/* eslint-disable no-constant-condition */
+import { take, put, call, fork, all } from 'redux-saga/effects';
 import { isEmpty } from 'lodash';
 
+import { SIGN_IN, SIGN_UP, LOGOUT } from '../actions/constants';
+
+function* checkSignIn(data) {
+  yield console.log(data);
+}
+
+function* checkSignUp(data) {
+  yield console.log(data);
+}
+
+function* watchSignIn() {
+  while (true) {
+    const { data } = yield take(SIGN_IN);
+    yield fork(checkSignIn, data);
+  }
+}
+
+function* watchSignUp() {
+  while (true) {
+    const { data } = yield take(SIGN_UP);
+    yield fork(checkSignUp, data);
+  }
+}
+
+export default function* watchAuth() {
+  yield fork(all, [fork(watchSignUp), fork(watchSignIn)]);
+}
--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -2,9 +2,10 @@
 import { fork, all } from 'redux-saga/effects';
 
 import watchNav from './navigation';
+import watchAuth from './authentication';
 
 function* rootSaga() {
-  yield all([fork(watchNav)]);
+  yield all([fork(watchNav), fork(watchAuth)]);
 }
 
 export default rootSaga;
