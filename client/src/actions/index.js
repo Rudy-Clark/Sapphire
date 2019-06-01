24092aa0c0888cfe9445dc5d219ce331a65315cd
Rudy Clark <rudy.uzb@gmail.com>
2019-06-01 10:13:21 +0500

    add actions user

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,11 @@
-import { REQUEST_ERROR, SET_PAGE } from './constants';
+import {
+  REQUEST_ERROR,
+  SET_PAGE,
+  SIGN_IN,
+  SIGN_UP,
+  ADD_USER,
+  DEL_USER,
+} from './constants';
 
 export const setReqError = error => ({
   type: REQUEST_ERROR,
@@ -8,4 +15,23 @@ export const setReqError = error => ({
 export const setContent = payload => ({
   type: SET_PAGE,
   payload,
+});
+
+export const signIn = data => ({
+  type: SIGN_IN,
+  data,
+});
+
+export const signUp = data => ({
+  type: SIGN_UP,
+  data,
+});
+
+export const addUser = payload => ({
+  type: ADD_USER,
+  payload,
+});
+
+export const delUser = () => ({
+  type: DEL_USER,
 });
