09caf7a44cfc0c3470a2d601f8d1011920bc02f3
Rudy Clark <rudy.uzb@gmail.com>
2019-06-01 08:31:09 +0500

    add local-storage

---
+++ client/src/api/local-storage.js
@@ -0,0 +1,28 @@
+import { isEmpty } from 'lodash';
+
+const keys = ['username', 'token'];
+
+const setItem = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getItem = key => JSON.parse(localStorage.getItem(key));
+
+const removeItem = key => localStorage.removeItem(key);
+
+export const setLocalStorage = data => {
+  keys.forEach(item => {
+    if (!isEmpty(data[item])) setItem(item, data[item]);
+  });
+};
+
+export const getLocalStorage = () => {
+  if (isEmpty(keys)) return null;
+  return keys.reduce((object, key) => {
+    // eslint-disable-next-line no-param-reassign
+    if (!isEmpty(getItem(key))) object[key] = getItem(key);
+    return object;
+  }, {});
+};
+
+export const removeLocalStorage = () => keys.forEach(key => removeItem(key));
