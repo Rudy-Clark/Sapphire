706bbba370a63c6b4759bc82ed2c7c07a3f5df8c
Rudy Clark <rudy.uzb@gmail.com>
2019-06-07 14:27:29 +0500

    add images migrations

---
+++ src/db/migrations/20190607142414_images.js
@@ -0,0 +1,27 @@
+exports.up = knex =>
+  knex.schema.createTable('images', table => {
+    table.increments().primary();
+    table
+      .integer('post_id')
+      .unsigned()
+      .notNullable();
+    table
+      .string('xs')
+      .notNullable()
+      .unique();
+    table
+      .string('md')
+      .notNullable()
+      .unique();
+    table
+      .string('md')
+      .notNullable()
+      .unique();
+      
+    table
+      .foreign('post_id')
+      .references('id')
+      .inTable('posts');
+  });
+
+exports.down = knex => knex.schema.dropTable('posts');
